619a72c5e35a6367f6eabd472a6a86a13862be20
Prakash Sharma <prakash009Kp@gmail.com>
2020-12-11 08:26:45 +0530

    Add splash navigation to home fragment

---
+++ app/src/main/java/com/androidprodev/mdc/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.androidprodev.mdc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.withTimeout
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/androidprodev/mdc/SplashFragment.kt
+++ app/src/main/java/com/androidprodev/mdc/SplashFragment.kt
@@ -1,5 +1,22 @@
 package com.androidprodev.mdc
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SplashFragment : Fragment(R.layout.fragment_splash)
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
+    }
+
+}
